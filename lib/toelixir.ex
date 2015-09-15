ce5208b9834b8d4b654c045a3446b8f69acd2844
hqmq <michael@riesd.com>
2015-09-15 15:29:29 -0600

    automate joining the game with hardcoded values

--- lib/toelixir.ex
+++ lib/toelixir.ex
@@ -11,10 +11,11 @@ defmodule Toelixir do
   end
 
   # Callbacks
-  def init([]), do: {:once, 1}
+  def init([]), do: {:once, %{role: nil, token: "me", name: "me", topic: "tictactoe:1"}}
 
   def onconnect(_wsreq, state) do
     IO.puts "connected! time to send the first message"
+    send_join_request(state)
     {:ok, state}
   end
 
@@ -24,13 +25,29 @@ defmodule Toelixir do
   end
 
   def websocket_handle({:text, msg}, _conn, state) do
-    IO.puts "Received text! #{inspect msg} (#{state})"
-    {:ok, state}
+    IO.puts "Recevied: #{msg}"
+    IO.puts "State: #{inspect state}"
+    msg = Poison.decode!(msg)
+    case msg do
+      %{"event" => "phx_reply", "payload" => %{"status" => "ok", "response" => %{"role" => assigned_role}}} ->
+        {:ok, %{state | role: assigned_role}}
+      %{"event" => "state", "payload" => %{"whose_turn" => role}} ->
+        case role == state.role do
+          true ->
+            IO.puts "It's my turn!"
+            {:ok, state}
+          false ->
+            {:ok, state}
+        end
+      _ ->
+        {:ok, state}
+    end
   end
 
   def websocket_info({:send, msg}, _connstate, state) do
-    IO.puts "sending #{inspect msg}"
-    {:reply, {:text, Poison.encode!(msg)}, state}
+    msg = Poison.encode!(msg)
+    IO.puts "sending: #{msg}"
+    {:reply, {:text, msg}, state}
   end
 
   def websocket_terminate(reason, _connstate, state) do
@@ -38,4 +55,11 @@ defmodule Toelixir do
     IO.inspect state
     :ok
   end
+
+  # Private Methods
+  defp send_join_request(%{token: token, name: name, topic: topic}) do
+    msg = %{topic: topic, event: "phx_join", ref: 1, payload: %{token: token, name: name}}
+    send self, {:send, msg}
+  end
+
 end
